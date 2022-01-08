b816b01de8955112897bfd20aaed97be68685e87
Aayush Regmi <aayush.181509@ncit.edu.np>
2022-01-08 10:28:24 +0545

    Newly added virtual paint code from another laptop

--- chapter3.py
+++ chapter3.py
@@ -5,9 +5,11 @@ import numpy as np
 img=cv2.imread('Resources/image1.jpg')
 print(img.shape)
 
-imgResize=cv2.resize(img,(500,1000))#Width *height
-print(imgResize.shape)
+# imgResize=cv2.resize(img,(500,1000))#Width *height
+print(img.shape)#height ,width , bgr value
+imgCropped=img[0:480,200:480]#height and width
 
 
-# cv2.imshow('Image',imgResize)
+
+cv2.imshow('Image',imgCropped)
 cv2.waitKey(0)
--- chapter4.py
+++ chapter4.py
@@ -1,11 +1,16 @@
 #Shapes and Text
 import cv2
 import numpy as np
-
-img=np.zeros((512,512,3),np.uint8)
+#Creating a simple black image using numpy array
+img=np.zeros((512,512,3),np.uint8)# 3 for giving color functionality
+# print(img.shape)
 # print(img)
+# img[:]=255,0,0 #To color image to blue(B.G,R)
 
-# cv2.line(img,(0,0),(img.shape))
-cv2.rect(img,(0,0),(250,350),(0,0,255),2)
+cv2.line(img,(0,0),(img.shape[1],img.shape[0]),(0,255,0),3)
+cv2.rectangle(img,(0,0),(250,350),(0,0,255),2)#cv2.FILLED to fill the rectangle
+cv2.circle(img,(400,50),30,(255,255,0),5)
+cv2.putText(img,"This is a Text",(300,300),cv2.FONT_HERSHEY_PLAIN,1,(0,150,0),1)
+
 cv2.imshow('Image',img)
 cv2.waitKey(0)
